5cd015e020f4938f81e30b4ca3730b562fa308ce
weijiafen <364886455@qq.com>
2018-01-05 09:55:11 +0800

    同步数据库文件 Signed-off-by: weijiafen <364886455@qq.com>

--- vuestore.sql
+++ vuestore.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-10-18 17:41:48
+Date: 2018-01-05 09:54:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,14 +28,16 @@ CREATE TABLE `category` (
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
   CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of category
 -- ----------------------------
-INSERT INTO `category` VALUES ('1', '冷菜', '1', null, null);
-INSERT INTO `category` VALUES ('2', 'asdda', '1', null, null);
-INSERT INTO `category` VALUES ('4', '招牌菜', '1', null, null);
+INSERT INTO `category` VALUES ('5', '优惠', '1', null, null);
+INSERT INTO `category` VALUES ('6', '双人套餐', '1', null, null);
+INSERT INTO `category` VALUES ('7', '主餐+饮料套餐', '1', null, null);
+INSERT INTO `category` VALUES ('8', '米粉米线', '1', null, null);
+INSERT INTO `category` VALUES ('9', '主食套餐', '1', null, null);
 
 -- ----------------------------
 -- Table structure for `good`
@@ -51,17 +53,22 @@ CREATE TABLE `good` (
   `isOnline` int(11) NOT NULL,
   `createAt` bigint(20) DEFAULT NULL,
   `updateAt` bigint(20) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `categoryId` (`categoryId`),
   CONSTRAINT `categoryId` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of good
 -- ----------------------------
-INSERT INTO `good` VALUES ('1', '凉拌青瓜', '好吃', '35', '1', '15', '0', null, null);
-INSERT INTO `good` VALUES ('2', '夫妻肺片', '好吃吃次', '11', '1', '18', '0', null, null);
-INSERT INTO `good` VALUES ('7', 'asd', 'asd', '111', '1', '12', '0', null, null);
+INSERT INTO `good` VALUES ('17', '双拼饭汤套餐配脆笋蒸蛋', '排骨拼鸡腿肉饭*1四季猪骨汤*1肉燥脆笋*1香滑蒸蛋*1', '12', '5', '26.25', '1', null, null, '/file/7616603086032818.jpg');
+INSERT INTO `good` VALUES ('18', '栗子鸡新品满足餐', '栗子焖土鸡饭*1四季猪骨汤*1蒸饺*1', '2', '5', '37.13', '1', null, null, '/file/4028130857117650.jpg');
+INSERT INTO `good` VALUES ('19', '超值双人餐', '香汁排骨饭*1香菇鸡腿饭*1咖喱鱼丸*1元气竹丝鸡汤*1四季猪骨汤*1香滑蒸蛋*1', '0', '5', '54', '1', null, null, '/file/9054985509261786.jpg');
+INSERT INTO `good` VALUES ('20', '超值双人餐', '香汁排骨饭*1香菇鸡腿饭*1咖喱鱼丸*1元气竹丝鸡汤*1四季猪骨汤*1香滑蒸蛋*1', '0', '6', '54', '1', null, null, '/file/1334921747542689.jpg');
+INSERT INTO `good` VALUES ('21', '鱼香茄子饭', '', '0', '7', '27', '1', null, null, '/file/7231204966550076.jpg');
+INSERT INTO `good` VALUES ('22', '瘦肉粉（小）', '瘦肉粉（小）*1', '0', '8', '9.5', '1', null, null, '/file/6698605017574994.jpg');
+INSERT INTO `good` VALUES ('23', '五香牛腩粉', '五香牛腩粉*1', '0', '8', '19', '1', null, null, '/file/3976726804314719.jpg');
 
 -- ----------------------------
 -- Table structure for `label`
@@ -77,15 +84,13 @@ CREATE TABLE `label` (
   PRIMARY KEY (`id`),
   KEY `goodId` (`goodId`),
   CONSTRAINT `goodId` FOREIGN KEY (`goodId`) REFERENCES `good` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of label
 -- ----------------------------
-INSERT INTO `label` VALUES ('6', '不要肺片', '#000', null, null, '2');
-INSERT INTO `label` VALUES ('7', '不要辣', '#EF1616', null, null, '1');
-INSERT INTO `label` VALUES ('8', '不要青瓜', '#25CF0F', null, null, '1');
-INSERT INTO `label` VALUES ('10', '加糖', '#F6F89B', null, null, '1');
+INSERT INTO `label` VALUES ('4', '加米饭', '#F4F8C5', null, null, '17');
+INSERT INTO `label` VALUES ('5', '加辣椒', '#E70C0C', null, null, '17');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -99,10 +104,13 @@ CREATE TABLE `user` (
   `lastLoginAt` bigint(20) DEFAULT NULL,
   `createAt` bigint(20) DEFAULT NULL,
   `updateAt` bigint(20) DEFAULT NULL,
+  `notice` varchar(255) DEFAULT NULL,
+  `openBusiness` int(11) NOT NULL DEFAULT '0',
+  `photo` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'weijiafen', '055d0ea123c9748b52804baef625e8e2', 'wade', '1508311094675', null, null);
+INSERT INTO `user` VALUES ('1', 'weijiafen', '055d0ea123c9748b52804baef625e8e2', '真功夫', '1508311094675', null, null, '欢迎光临真功夫', '0', '/file/8705401371272582.jpg');
