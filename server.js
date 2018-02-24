1e9854e6b0066a4e4cbac97c5ab2b59f97fb5a89
weijiafen <364886455@qq.com>
2018-02-24 17:11:38 +0800

    gzip Signed-off-by: weijiafen <364886455@qq.com>

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var compression = require('compression')
 var app = express();
+app.use(compression());
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 var fs=require('fs');
@@ -7,6 +9,7 @@ var router=require('./server/router.js');
 app.use(express.static('dist',{maxAge:36000000}));
 app.use(express.static('upload',{maxAge:36000000}));
 app.use(express.static('./',{maxAge:36000000}));
+
 server.listen(80);
 router(app);
 // app.use('/upload/file', express.static('upload/file'));
