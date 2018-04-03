3caa2845ba81eac903b69f9fefbd5fc7ca6f9557
weijiafen <364886455@qq.com>
2018-04-03 18:48:37 +0800

    库存秒杀单元测试 Signed-off-by: weijiafen <364886455@qq.com>

--- testRunner.js
+++ testRunner.js
@@ -1,2 +1,12 @@
-var unitTest=require('./mb4.js');
-console.log(unitTest())
+var unitTest=require('./unitTest.js');
+	for(let i=0;i<999;i++){
+		setTimeout(function(){
+			console.log("step"+i)
+			unitTest(1)
+		},0)
+		// setTimeout(function(){
+		// 	console.log("step"+i)
+		// 	unitTest(2)
+		// },0)
+	}
+return 
--- unitTest.js
+++ unitTest.js
@@ -1,48 +1,98 @@
-var crypto = require('crypto')
-var request = require('request');
-var parseString = require('xml2js').parseString;
-module.exports=((function(){
-	var xml=`<xml>
-	        <appid>wxa9c22df153e7dd7b</appid>
-	        <body>魏记小店-日用杂货</body>
-	        <mch_id>1497990382</mch_id>
-	        <nonce_str>abcdefg</nonce_str>
-	        <notify_url>http://www.yslpartition.com/customer/payCallBack</notify_url>
-	        <openid>oyMqK1jeRt6RQWlrF-3V7ZLch9S0</openid>
-	        <out_trade_no>274</out_trade_no>
-	        <spbill_create_ip>218.17.157.119</spbill_create_ip>
-	        <total_fee>150</total_fee>
-	        <trade_type>JSAPI</trade_type>
-	        <sign>7C6334B4F56653D5C79D3AC5A3E3B5AC</sign>
-	</xml>`
-	var data={
-		appid:'wxa9c22df153e7dd7b',
-		body:'魏记小店-日用杂货',
-		mch_id:'1497990382',
-		nonce_str:'abcdefg',
-		notify_url:'http://www.yslpartition.com/customer/payCallBack',
-		openid:'oyMqK1jeRt6RQWlrF-3V7ZLch9S0',
-		out_trade_no:271,
-		spbill_create_ip:'218.17.157.119',
-		total_fea:150,
-		trade_type:'JSAPI',
-		sign:''
-	}
-	request.post({
-		url:'https://api.mch.weixin.qq.com/pay/unifiedorder',
-		headers:{
-			"Content-type":"text/xml;charset=UTF-8"
-		},
-		body:xml,
-	},function(error,res1,body1){
-		parseString(body1, function (err, result2) {
-		    body1 = result2;
-			console.log("prePay callback",body1.xml.return_code[0])
-			// result.status=0
-			// result.msg='拉取支付号成功'
-			// result.data=body1
-			// response.end(JSON.stringify(result))
-		});
-		return body1
-	})
+var Sequelize=require('sequelize')
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var dbConfig=require('./server/connection/dbConfig.js')
+var sequelize = new Sequelize('test', dbConfig.user, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: 'mysql',
+  pool: dbConfig.pool,
+});
+var Good = sequelize.define('good', {
+  id:{
+    type:Sequelize.INTEGER,
+    primaryKey:true,
+    autoIncrement: true
+  },
+  count:{
+    type:Sequelize.INTEGER
+  },
+  createAt:{
+    type:Sequelize.BIGINT
+  },
+  updateAt:{
+    type:Sequelize.BIGINT
+  }
+}, {
+  freezeTableName: true, // Model tableName will be the same as the model name
+  timestamps: false
+});
+var Order = sequelize.define('order', {
+  id:{
+    type:Sequelize.INTEGER,
+    primaryKey:true,
+    autoIncrement: true
+  },
+  goodId:{
+  	type:Sequelize.INTEGER
+  },
+  count:{
+    type:Sequelize.INTEGER
+  },
+  createAt:{
+    type:Sequelize.BIGINT
+  },
+  updateAt:{
+    type:Sequelize.BIGINT
+  }
+}, {
+  freezeTableName: true, // Model tableName will be the same as the model name
+  timestamps: false
+});
+module.exports=(async(function(goodId){
+	var trans;
+	await(sequelize.transaction()
+		.then(async(function (t) {
+			trans=t
+			var goodRes=await(Good.findOne({
+				where:{
+					id:goodId
+				},
+				transaction:trans
+			}))
+			if(goodRes.dataValues.count<=0){
+				throw new Error
+			}else{
+				var newGood=await(Good.update({
+					count:goodRes.dataValues.count-1
+				},{
+					where:{
+						id:goodId,
+						count:{
+							$gte:0
+						}
+					},
+					transaction:trans
+				}))
+				if (newGood==0) {
+					throw new Error
+				}else{
+					return Order.create({
+						count:1,
+						goodId:goodId
+					}, {transaction: trans})
+				}
+				
+			}
+			
+		}))
+		.then(function(){
+			console.log("commit！！！！！！")
+			return trans.commit();
+		})
+		.catch(function(err){
+			console.log("rollback！！！！！！！！！")
+			return trans.rollback();
+		})
+	)
+	return
 }))
