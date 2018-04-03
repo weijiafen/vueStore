539468c228fbbf3e234d465b5512fc6b7395713f
weijiafen <364886455@qq.com>
2018-04-03 11:05:11 +0800

    验证码只输出数字 Signed-off-by: weijiafen <364886455@qq.com>

--- server/controler/captcha.js
+++ server/controler/captcha.js
@@ -1,6 +1,9 @@
 var svgCaptcha = require('svg-captcha');
 module.exports=function(req,res){
-	var captcha = svgCaptcha.create();
+	var captcha = svgCaptcha.create({
+		ignoreChars:'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+		noise:0
+	});
 	req.session.captcha = captcha.text;
 	res.set('Content-Type', 'image/svg+xml');
 	res.status(200).send(captcha.data);
